ca5ba4bbc87443c13925c418bbd9411abfbd2bdb
dirceuovbelem <dirceu@fourtime.com>
2015-05-06 14:43:10 -0300

    Implementando FullScreen

--- app/src/main/java/dirceubelem/exemplo5/SplashActivity.java
+++ app/src/main/java/dirceubelem/exemplo5/SplashActivity.java
@@ -1,6 +1,8 @@
 package dirceubelem.exemplo5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 
 
@@ -10,6 +12,23 @@ public class SplashActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        abrirHome();
+    }
+
+    private void abrirHome() {
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 
 }
